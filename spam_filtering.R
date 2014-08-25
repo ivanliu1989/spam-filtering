7659da67218cb2245b90b531b74a22d86f246cb5
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-08-25 13:48:22 +1000

    setup env

---
+++ spam_filtering.R
@@ -0,0 +1,11 @@
+setwd('C:\\Documents and Settings\\Macro\\Desktop\\Ivandata\\spam-filtering')
+library('tm')
+library('ggplot2')
+spam.path <- file.path("data", "spam")
+spam2.path <- file.path("data", "spam_2")
+easyham.path <- file.path("data", "easy_ham")
+easyham2.path <- file.path("data", "easy_ham_2")
+hardham.path <- file.path("data", "hard_ham")
+hardham2.path <- file.path("data", "hard_ham_2")
+
+# Create motivating plot
